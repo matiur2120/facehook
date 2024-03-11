ac415cc186187b4c21c9f1c832e13cf8784f0b50
matiur2120 <matiur211993@gmail.com>
2024-03-12 01:52:25 +0600

    Complete login functinality with jwt and refresh-token

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
@@ -9,16 +10,35 @@ const LoginForm = () => {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm();
 
-  const submitForm = (formData) => {
+  const submitForm = async (formData) => {
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_SERVER_BASE_URL}/auth/login`,
+        formData
+      );
+      if (response.status === 200) {
+        const { token, user } = response.data;
+        if (token) {
+          const authToken = token.token;
+          const refreshToken = token.refreshToken;
+          console.log(`Login time auth token ${authToken}`);
+          setAuth({ user, authToken, refreshToken });
+          navigate("/");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      setError("root.random", {
+        type: "random",
+        message: `User with ${formData.email} is not found!`,
+      });
+    }
+
     //Make a API call
     //will return Tokens and logged in user information
-    const user = { ...formData };
-    setAuth({ user });
-
-    console.log(formData);
-    navigate("/");
   };
   return (
     <form
@@ -53,6 +73,7 @@ const LoginForm = () => {
           id="password"
         />
       </Field>
+      <p>{errors?.root?.random?.message}</p>
       <Field>
         <button
           className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,42 @@
+import { useEffect, useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import useAxios from "../hooks/useAxios";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const { auth } = useAuth();
+  const { api } = useAxios();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchProfile = async () => {
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/profile/${auth?.user?.id}`
+        );
+        setUser(response?.data?.user);
+        setPosts(response?.data?.posts);
+      } catch (error) {
+        console.log(error);
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProfile();
+  }, []);
+  if (loading) {
+    return <div>Featching your profile data......</div>;
+  }
+  return (
+    <div>
+      Welcome, {user?.firstName} {user?.lastName}
+      <p>You have {posts.length} posts</p>
+    </div>
+  );
 };
 
 export default ProfilePage;
